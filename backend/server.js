7b35d922f391ca744a9fbefed249b477abff4b62
fanelob46 <fanelob46@gmail.com>
2024-11-27 06:11:26 +0200

    error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser())
+
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(5000, () => {
   connectDB();
