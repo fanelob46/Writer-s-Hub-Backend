22533c3cc7e9e921ebb86f7f9a34d24342aac3e1
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-30 21:02:28 +0200

    login user

--- backend/controllers/users/login.js
+++ backend/controllers/users/login.js
@@ -1,3 +1,21 @@
-export const loginUser = async (req,res) => {
+import User from "../../models/userModel.js";
+import generateToken from "../../utils/generateToken.js";
 
-}
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      success: true,
+      message: "Successfully logged into Writers HUb. Welcome!",
+      data: user,
+    });
+  } else {
+    res.status(401);
+     throw new Error("Invalid email or password");
+  }
+};
