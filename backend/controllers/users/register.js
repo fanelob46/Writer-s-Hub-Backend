35edaf96751e12357000ffeadc02beb88b6ea1c0
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-30 20:22:32 +0200

    Register user

--- backend/controllers/users/register.js
+++ backend/controllers/users/register.js
@@ -1,3 +1,29 @@
-export const registerUser = async (req,res) => {
-    
-}
+import User from "../../models/userModel.js";
+import asyncHandler from "express-async-handler";
+
+// @desc user register
+// @route POST users/
+// access PUBLIC
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const registerFormData = req.body;
+  const { email, username } = req.body;
+
+  const userExists = await User.findOne({ email, username });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  
+  const createdUser = await User.create(registerFormData);
+  if (createdUser) {
+    res.status(201).json({
+      success: true,
+      message: "Successfully registered to Writers HUb. Welcome!",
+      data: createdUser,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Unfortunately your registration was unsuccessful");
+  }
+});
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
 import cookieParser from "cookie-parser";
-import userRoutes from './routes/userRoutes.js'
-import contentRoutes from './routes/contentRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
+import contentRoutes from "./routes/contentRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/content", contentRoutes);
