1a628ce174816e9373475d4851ec5d2a87c3529d
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-12-01 00:45:41 +0200

    Get all content Done

--- backend/controllers/content/getAllContent.js
+++ backend/controllers/content/getAllContent.js
@@ -1,3 +1,24 @@
-export const getAllContent = async (req,res) => {
-    
-}
+import expressAsyncHandler from "express-async-handler";
+import Content from '../../models/contentModel.js'
+
+export const getAllContent = expressAsyncHandler(async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = 20;
+  const skip = (page - 1) * limit;
+
+  const contents = await Content.find({}).skip(skip).limit(limit);
+
+  const totalContents = await Content.countDocuments({});
+
+  if (contents.length > 0) {
+    res.status(200).json({
+      data: contents,
+      page,
+      pages: Math.ceil(totalContents / limit),
+      totalContents
+    });
+  } else {
+    res.status(404);
+    throw new Error('No content found');
+  }
+})
